2a4c9f1a7d051d10271e921b8228f47f058bed9b
jl1405 <119141346+jl1405@users.noreply.github.com>
2026-05-15 15:13:27 -0500

    base 8
    registro good

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://evbltjdiptevkximgpy.supabase.co';
+const SUPABASE_URL = 'https://evbltjdiptevklximgpy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Ymx0amRpcHRldmtseGltZ3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTkwMTMsImV4cCI6MjA5NDQzNTAxM30.ACfrgKI5m5iAhLvv16njK-vjQm04tbSOdWbR-G5ABIM';
 
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
